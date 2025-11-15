cf417818b4f196cb46c976c314a4ad6afb5bc6f1
Mikko Pirhonen <mikkopirho@gmail.com>
2025-11-15 17:43:19 +0200

    Update

--- run_onchange_2_install-fonts.sh
+++ run_onchange_2_install-fonts.sh
@@ -2,7 +2,7 @@
 
 TMPDIR=/tmp/fonts
 mkdir -p $TMPDIR
-for FONT_NAME in CascadiaCode JetBrainsMono SourceCodePro IntelOneMono; do
+for FONT_NAME in CascadiaCode JetBrainsMono SourceCodePro IntelOneMono Overpass; do
   ZIPFILE="$TMPDIR/fonts.zip"
 
   INSTDIR="$TMPDIR/$FONT_NAME"
@@ -11,7 +11,7 @@ for FONT_NAME in CascadiaCode JetBrainsMono SourceCodePro IntelOneMono; do
   sudo mkdir -p $SYSDIR
   sudo mkdir -p $INSTDIR
 
-  wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$FONT_NAME.zip" -O "$ZIPFILE"
+  wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/$FONT_NAME.zip" -O "$ZIPFILE"
   sudo unzip "$ZIPFILE" -d "$INSTDIR"
   rm "$ZIPFILE"
   sudo mv $INSTDIR/* $SYSDIR
